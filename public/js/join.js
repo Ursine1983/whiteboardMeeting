74bd47c51a3a5758468e053e289799d25a5de7b4
Sebastian Baumruker <sebastian.baumruker@gmail.com>
2020-10-21 10:03:16 +0200

    relative redirect path

--- public/js/join.js
+++ public/js/join.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     socket.on('joined', function(data) {
-        window.location = generateUrl('file:///C:/xampp/htdocs/digiWhiteboard/public/chat.html', data);
+        var path = window.location.href,
+            afterWith = path.substr(0, path.lastIndexOf("/") + 1);
+
+        window.location = generateUrl(afterWith + 'chat.html', data);
     });
 });
