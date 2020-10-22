cdb3130058dba4bedb31709f58110aca96e4587e
Sebastian Baumruker <sebastian.baumruker@gmail.com>
2020-10-22 11:53:53 +0200

    load config fix_03

--- public/js/connect.js
+++ public/js/connect.js
@@ -1,5 +1,5 @@
 function connect() {
-    fetch("config/connection.json")
+    fetch("../config/connection.json")
         .then(function(response){
             var config = response.json(),
                 socket  = io.connect(config.url + ':8080');
